011d266118464c00f2488aa9b4f7510089970103
techyjc <john@techbyte.co.uk>
2025-01-04 18:21:15 +0000

    Updated and revised code!

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ let screxpanded = calc_display.getAttribute("aria-expanded");
 
 let calc_logentries = document.querySelector(".log-entries");
 
-let btnvalue = "";
+let keyValue = "";
+let keyDown = false;
 
-const acceptedkeys = [
+const acceptedKeys = [
     "0",
     "1",
     "2",
@@ -35,43 +36,43 @@ const acceptedkeys = [
     "C",
     "Enter"
 ];
-const acceptedops = ["+", "-", "*", "/", "%", "=", "Enter"];
+const acceptedOps = ["+", "-", "*", "/", "%", "=", "Enter"];
+const acceptedfunctkeys = ["Alt", "Shift", "Control", "Meta"]
+
+let calcSum = [];
+let enteredSum = '';
+let displaySum = '';
+let resultSum = 0;
+let clearCount = 0;
 
 calc_input.readOnly = true;
 
+let myargs = { Key: '', keyFunc: '', keyHold: false, isKeyboard: false };
 
 document.addEventListener("keydown", (e) => {
-    if (e.key == "Alt") {
-        keyhold = true;
+    if (e.key == "Alt" || e.key == "Shift" || e.key == "Shift") {
+        args.keyHold = true;
+        args.keyFunc = e.key;
     }
 });
 
 // Event Listeners
 
 document.addEventListener("keyup", (e) => {
-    if (e.key == "Alt") {
-        keyhold = false;
-    }
+    myargs.keyHold = false;
 });
 
 
 document.addEventListener("keypress", (e) => {
-    if (acceptedkeys.includes(e.key)) {
-        entry(e.key, keyhold, true);
-    }
+        myargs.isKeyboard = true;
+        myargs.Key = e.key;
+        userEntry(myargs);
 });
 
 calc_buttons.addEventListener("click", (e) => {
-    if (e.target.classList.contains("calc-num")) {
-        btnvalue = parseInt(e.target.getAttribute("data-button"));
-        entry(btnvalue, keyhold);
-    }
-
-    if (e.target.classList.contains("calc-func")) {
-        btnvalue = e.target.getAttribute("data-button");
-        if (btnvalue != "") {
-            entry(btnvalue, keyhold);
-        }
+    if (e.target.classList.contains("calc-btn")) {
+        myargs.Key = e.target.getAttribute("data-button").toString();
+        userEntry(myargs);
     }
 });
 
@@ -84,6 +85,79 @@ calc_logentries.addEventListener("click", (e) => {
 });
 
 calc_clear.addEventListener("click", (e) => {
-
 });
 
+function isOperator(args) {
+    if (acceptedOps.includes(args.Key)) {
+        return true;
+    }
+}
+
+function userEntry(args) {
+    if(args.Key == "Enter") { args.Key = "="}
+    if(enteredSum.substring(enteredSum.length -1) == "." && args.Key == "."){ return; }
+    if(enteredSum == "0" && args.Key == "0"){ return; }
+    if(enteredSum === "" && args.Key === "="){ return; }
+
+    if(args.Key == '.' && enteredSum == '' ){
+        enteredSum = '0.';
+        displaySum = '0.';
+        calcDisplay(displaySum);
+        return;
+    }
+    
+    if(args.Key == 'c') {
+        clear();
+        clearCount++;
+        if(clearCount == 2) {
+            clear(true);
+            clearCount = 0;
+        }
+        return;
+    }
+
+    clearCount = 0 ; // Reset clearCount;
+
+    if (isOperator(args)) {
+        if(args.Key == "="){
+            calcSum.push(enteredSum);
+            enteredSum = '';
+            resultSum = calcSum.join('');
+
+            calcDisplay(eval(resultSum));
+            clear();
+            return;
+        }
+
+        calcSum.push(enteredSum);
+        enteredSum = '';
+    }
+   
+    if (acceptedKeys.includes(args.Key)) {
+        enteredSum += args.Key;
+        displaySum += args.Key; //Used to display user entry.
+    }
+
+    calcDisplay(displaySum);
+}
+
+function calcDisplay(output){
+    if(output !=""){
+        calc_display.value = output;
+    }
+    
+}
+
+function clear(clearDisplay=false) {
+    calcSum = [];
+    displaySum = '';
+    enteredSum = '';
+    if(clearDisplay){
+        calcDisplay('0');
+    }
+    console.clear;
+}
+
+function testOutput(output) {
+    console.log(output);
+}
